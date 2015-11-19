128e4024fd05de9675d9b28bcb9434082fab1d94
ellenschwartau <ellen.schwartau@hotmail.de>
2015-11-19 11:53:52 +0100

    verteilte systeme protbuffers

--- src/de/fhwedel/ueb04/SocketClient.java
+++ src/de/fhwedel/ueb04/SocketClient.java
@@ -10,22 +10,46 @@ import de.fhwedel.ueb04.LogMessageProtos.LogMessage;
 import de.fhwedel.ueb04.LogMessageProtos.LogMessage.ID;
 import de.fhwedel.ueb04.LogMessageProtos.LogMessage.Priority;
 
+/**
+ * Socket Client der LogNachrichten an einen Server schicken kann.
+ */
 public class SocketClient {
 
+    private static final String MALTES_SERVER = "172.26.0.126";
+    private static final int MALTES_PORT = 1337;
+
+    private static final String SERVER = "127.0.0.1";
+    private static final int PORT = 5000;
+
+    /**
+     * Stellt eine Verbindung zum Server her und verschickt eine Log-Nachricht.
+     * 
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         Socket s = new Socket();
+        connect(s, SERVER, PORT);
+        LogMessage message = LogMessage.newBuilder()
+                .setContent("Test Message")
+                .setPriority(Priority.INFO)
+                .setTimestamp(new Date().toString())
+                .setId(ID.newBuilder().setAutor("Ellen").setLocation("Hamburg").build())
+                .build();
+        message.writeDelimitedTo(s.getOutputStream());
+        s.close();
+    }
 
+    /**
+     * Stellt eine Verbindung zum Server her.
+     * 
+     * @param s
+     * @throws IOException
+     */
+    private static void connect(Socket s, String server, int host) throws IOException {
         try {
-            s.connect(new InetSocketAddress("localhost", 5000));
+            s.connect(new InetSocketAddress(server, host));
             System.out.println("Connected");
-
-            LogMessage message = LogMessage.newBuilder()
-                    .setContent("Test Message")
-                    .setPriority(Priority.INFO)
-                    .setTimestamp(new Date().toString())
-                    .setId(ID.newBuilder().setAutor("Ellen").setLocation("Hamburg").build())
-                    .build();
-            message.writeTo(s.getOutputStream());
 
         } catch (UnknownHostException e) {
             System.err.println("Cant't find Host");
--- src/de/fhwedel/ueb04/SocketServer.java
+++ src/de/fhwedel/ueb04/SocketServer.java
@@ -1,61 +1,117 @@
 package de.fhwedel.ueb04;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import de.fhwedel.ueb04.LogMessageProtos.LogMessage;
 
+/**
+ * Einfacher Socket-Server der auf eingehende Verbindungen wartet und Meldungen in ein Logfile
+ * schreibt.
+ */
 public class SocketServer {
 
+    private static final String LOGFILE_PATH = "./src/de/fhwedel/ueb04/messageLog.log";
     private static PrintStream out = null;
     private static ServerSocket s = null;
     private static Socket connection = null;
     private static BufferedReader in = null;
 
+    /**
+     * Wartet auf eingehende Verbindungen und logged eingehende Log-Meldungen in ein Logfile.
+     * 
+     * @param args
+     */
     public static void main(String args[]) {
+        try {
+            startConnection();
+            parseInput(connection.getInputStream());
+        } catch (IOException e) {
+            System.err.println("Input konnte nicht gelesen werden: ");
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+    }
+
+    /**
+     * Nimmt eine eingehende Verbindung entgegen und verarbeitet den Input.
+     * 
+     * @throws IOException
+     */
+    private static void startConnection() {
 
         try {
             s = new ServerSocket(5000, 10);
-            echo("Server socket erstellt. Verbindungsaufbau...");
+            System.out.println("Server socket erstellt. Verbindungsaufbau...");
             connection = s.accept();
-            echo("Verbindung aufgebaut von: " + connection.getInetAddress().getHostName() + " : "
+            System.out.println("Verbindung aufgebaut von: "
+                    + connection.getInetAddress().getHostName()
+                    + " : "
                     + connection.getPort());
 
             out = new PrintStream(connection.getOutputStream());
             out.print(new String("Connected to server.").toCharArray());
             out.flush();
-
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            while (true) {
-                parseInput(in);
-            }
-
         } catch (IOException e) {
-            System.err.println("IOException");
+            System.out.println("Verbindung konnte nicht aufgebaut werden.");
         }
+    }
 
+    /**
+     * Schließt die aufgebaute Verbindung.
+     */
+    private static void closeConnection() {
         try {
-            in.close();
-            out.close();
-            s.close();
+            if (in != null)
+                in.close();
+            if (out != null)
+                out.close();
+            if (s != null)
+                s.close();
         } catch (IOException ioException) {
             System.err.println("Verbindung konnte nicht geschlossen werden.");
         }
     }
 
-    public static void echo(String msg) {
-        System.out.println(msg);
+    /**
+     * Parsed den Input.
+     * 
+     * @param inputStream
+     * @throws IOException
+     */
+    private static void parseInput(InputStream inputStream) {
+        try {
+            LogMessage logMessage = LogMessage.parseDelimitedFrom(inputStream);
+            writeToLogFile(logMessage, LOGFILE_PATH);
+        } catch (IOException e) {
+            System.out.println("LogMessage konnte nicht geparsed werden: ");
+            e.printStackTrace();
+        }
     }
 
-    private static void parseInput(BufferedReader in) {
-
-    }
-
-    private static void writeToLogFile(LogMessage message) {
-
+    /**
+     * Schreibt die emfpangene LogMessage in das Logfile.
+     * 
+     * @param filePath Pfad zur Log-Datei
+     * @param message zu schreibende Nachricht
+     */
+    private static void writeToLogFile(LogMessage message, String filePath) {
+        FileWriter logFileWriter;
+        try {
+            logFileWriter = new FileWriter(new File(filePath), true);
+            System.out.println("Schreibe Nachricht '" + message.toString() + "' in Datei "
+                    + filePath);
+            logFileWriter.write(message.toString());
+            logFileWriter.flush();
+        } catch (IOException e) {
+            System.out.println("Datei konnte nicht gefunden werden.");
+        }
     }
 }
